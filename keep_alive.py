0ecc0503b42385a5ada5e5ad5e5a804967c6db52
Glitchood <105179164+Glitchood@users.noreply.github.com>
2024-01-01 04:06:21 +0000

    Added <h1></h1> to keep alive text

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask('Groovebot')
 
 @app.route('/')
 def home():
-    return "GrooveBot alive"
+  return "<h1>GrooveBot Online!</h1>"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
