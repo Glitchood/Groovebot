bed48d26bd8e4d8b147aaa9857a6fc7671ac8a21
Glitchood <105179164+Glitchood@users.noreply.github.com>
2024-01-04 06:49:12 +0000

    Tried to make it work with replit deployments😭 no luck

---
+++ pub/app.py
@@ -0,0 +1,57 @@
+import os
+from flask import Flask, render_template, request, redirect, session
+from zenora import APIClient
+from threading import Thread
+
+BOT_TOKEN = os.environ['CLIENT_TOKEN']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+OAUTH_URL = os.environ['OAUTH_URL']
+REDIRECT_URI = os.environ['REDIRECT_URI']
+
+app = Flask(__name__, template_folder='/pub')
+client = APIClient(BOT_TOKEN, client_secret=CLIENT_SECRET)
+
+app.config["SECRET_KEY"] = "verysecret69"
+
+
+@app.route("/")
+def home():
+    access_token = session.get("access_token")
+
+    if not access_token:
+        return render_template("index.html")
+
+    bearer_client = APIClient(access_token, bearer=True)
+    current_user = bearer_client.users.get_current_user()
+
+    return render_template("index.html", user=current_user)
+
+
+@app.route("/login")
+def login():
+    return redirect(OAUTH_URL)
+
+
+@app.route("/logout")
+def logout():
+    session.pop("access_token")
+    return redirect("/")
+
+@app.route("/oauth/callback")
+def oauth_callback():
+    arguments = request.args
+    if not 'error' in arguments:
+      code = arguments['code']
+      access_token = client.oauth.get_access_token(
+          code, redirect_uri=REDIRECT_URI).access_token
+      session["access_token"] = access_token
+      return redirect("/")
+    else:
+      return redirect("/")
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
